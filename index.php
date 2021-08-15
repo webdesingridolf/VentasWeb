6b57b7b7c4e8b1e19670f69d4fb07da522c8abd6
webdesingridolf <amoya.181ci24@istta.edu.pe>
2021-08-14 19:04:29 -0500

    listo

--- index.php
+++ index.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+    <ul>
+        <div>
+        <img src="img/LogoSAM" alt="" class="logo">
+        </div>
+        
+        <div>
+        <li><a href="">inicio</a></li>
+        <li><a href="">productos</a></li>
+        </div>
+        
+    </ul>
     
 </body>
 </html>
