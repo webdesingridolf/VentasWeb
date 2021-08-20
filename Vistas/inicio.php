0c312782372f6fbb0d6dd578b6eefbd65804be71
webdesingridolf <amoya.181ci24@istta.edu.pe>
2021-08-20 11:28:55 -0500

    nuevo inicio

---
+++ Vistas/inicio.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <div>
+        <ul>
+            <div>
+                <li>
+                    <img src="../img/LogoSAM" alt="">
+                </li>
+            </div>
+            <div>
+                <li>
+                    <a href="">Inicio</a>
+                </li>
+            </div>
+            <div>
+                <li>
+                    <a href="">Productos</a>
+                </li>
+            </div>
+            
+        </ul>
+    </div>
+    
+</body>
+</html>
