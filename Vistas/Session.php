b5d13b88e114708fc51bf54a187127d43c320ca8
Ricardo <rncliestpta@gmail.com>
2021-09-25 10:21:09 -0500

    session

---
+++ Vistas/Session.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/Session.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <img src="" alt="img"></br>
+        <label for="usuario">IdUsuario</label>   
+        <input type="text" name="usuario" id=""> <br>
+        <label for="pasword">Password</label>
+        <input type="text" name="pasword" id=""> <br>
+        <input type="submit" value="Ingresar">
+        <p>registrese aqui <a href="Registrate.html">registrate</a></p>
+        
+    </form>
+</body>
+</html>
---
+++ Vistas/Tabla.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/Tabla.css">
+    <title>Tablas</title>
+</head>
+<body>
+    <table >
+        <thead>
+            <tr>
+                <th>Usuario</th>    
+                <th>Password</th>
+                <th>E-mailss</th>
+            </tr>
+            
+        </thead>
+        <tbody>
+            <tr>
+                <td>ricc</td>
+                <td>passsss</td>
+                <td>ejemplo@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ricc</td>
+                <td>passsss</td>
+                <td>ejemplo@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ricc</td>
+                <td>passsss</td>
+                <td>ejemplo@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ricc</td>
+                <td>passsss</td>
+                <td>ejemplo@gmail.com</td>
+            </tr>
+            <tr>
+                <td>ricc</td>
+                <td>passsss</td>
+                <td>ejemplo@gmail.com</td>
+            </tr>
+        </tbody>
+
+    </table>
+
+    
+</body>
+</html>
